9f4bfe58979317166c2f316919d737444b7a4492
Peter <szpeter@Yeah.net>
2024-04-27 23:38:24 +0800

    Add DependencyProperty register exception

---
+++ 6-DependencyPropertyType/6-DependencyPropertyType-CPP/Interpolation.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "Interpolation.h"
+#if __has_include("Interpolation.g.cpp")
+#include "Interpolation.g.cpp"
+#endif
+#include <winrt/Windows.UI.Xaml.Interop.h>
+
+namespace winrt::_6_DependencyPropertyType_CPP::implementation
+{
+    winrt::Microsoft::UI::Xaml::DependencyProperty Interpolation::LinearProperty()
+    {
+        static auto s_linearProperty =
+            winrt::Microsoft::UI::Xaml::DependencyProperty::RegisterAttached(
+                L"Linear",
+                winrt::xaml_typename<winrt::Microsoft::UI::Xaml::DependencyProperty>(),
+                winrt::xaml_typename<class_type>(),
+                { nullptr }
+        );
+        return s_linearProperty;
+    }
+    void Interpolation::SetLinear(winrt::Microsoft::UI::Xaml::UIElement element, winrt::Microsoft::UI::Xaml::DependencyProperty value)
+    {
+        element.SetValue(LinearProperty(), value);
+    }
+    winrt::Microsoft::UI::Xaml::DependencyProperty Interpolation::GetLinear(winrt::Microsoft::UI::Xaml::UIElement element)
+    {
+        return element.GetValue(LinearProperty()).as<winrt::Microsoft::UI::Xaml::DependencyProperty>();
+    }
+}
---
+++ 6-DependencyPropertyType/6-DependencyPropertyType-CPP/Interpolation.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "Interpolation.g.h"
+
+namespace winrt::_6_DependencyPropertyType_CPP::implementation
+{
+    struct Interpolation : InterpolationT<Interpolation>
+    {
+        Interpolation() = default;
+
+        static winrt::Microsoft::UI::Xaml::DependencyProperty LinearProperty();
+        static void SetLinear(
+            winrt::Microsoft::UI::Xaml::UIElement element,
+            winrt::Microsoft::UI::Xaml::DependencyProperty value
+        );
+        static winrt::Microsoft::UI::Xaml::DependencyProperty GetLinear(
+            winrt::Microsoft::UI::Xaml::UIElement element
+        );
+    };
+}
+
+namespace winrt::_6_DependencyPropertyType_CPP::factory_implementation
+{
+    struct Interpolation : InterpolationT<Interpolation, implementation::Interpolation>
+    {
+    };
+}
